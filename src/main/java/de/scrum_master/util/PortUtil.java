527a08d2966e1eec32162a68055695fe50b54564
Alexander Kriegisch <Alexander@Kriegisch.name>
2013-10-21 16:36:01 +0200

    Cosmetics: code style settings and auto-reformatting

--- src/main/java/de/scrum_master/util/PortUtil.java
+++ src/main/java/de/scrum_master/util/PortUtil.java
@@ -8,7 +8,6 @@ import java.net.ServerSocket;
  * Provides utility methods for checking availability of local TCP/UDP ports
  */
 public class PortUtil {
-
 	public static final int MIN_PORT_NUMBER = 1024;
 	public static final int MAX_PORT_NUMBER = 65535;
 	public static final int MAX_PORT_OFFSET = MAX_PORT_NUMBER - MIN_PORT_NUMBER;
@@ -18,9 +17,8 @@ public class PortUtil {
 	protected PortUtil() {}
 
 	public static PortUtil getInstance() {
-		if (instance == null) {
+		if (instance == null)
 			instance = new PortUtil();
-		}
 		return instance;
 	}
 
@@ -32,9 +30,8 @@ public class PortUtil {
 	 * @throws IllegalArgumentException if port is not in range MIN_PORT_NUMBER..MAX_PORT_NUMBER
 	 */
 	public boolean isAvailable(int port) {
-		if (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER) {
+		if (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER)
 			throw new IllegalArgumentException("port out of range " + MIN_PORT_NUMBER + ".." + MAX_PORT_NUMBER);
-		}
 		ServerSocket serverSocket = null;
 		DatagramSocket datagramSocket = null;
 		try {
@@ -44,24 +41,21 @@ public class PortUtil {
 			datagramSocket.setReuseAddress(true);
 			return true;
 		} catch (IOException e) {
-			// todo... nothing to do it is not free
+			return false;
 		} finally {
-			if (datagramSocket != null) {
+			if (datagramSocket != null)
 				datagramSocket.close();
-			}
-
 			if (serverSocket != null) {
 				try {
 					serverSocket.close();
 				} catch (IOException e) {}
 			}
 		}
-
-		return false;
 	}
 
 	/**
 	 * Checks all non-privileged TCP/UDP ports in range MIN_PORT_NUMBER..MAX_PORT_NUMBER for availability
+	 *
 	 * @return the first free port within the search range
 	 * @throws IOException if no port is available at all
 	 */
@@ -71,9 +65,10 @@ public class PortUtil {
 
 	/**
 	 * Checks all non-privileged TCP/UDP ports in range MIN_PORT_NUMBER + offset..MAX_PORT_NUMBER for availability
+	 *
 	 * @param offset number of ports above MIN_PORT_NUMBER to be skipped during availability check
 	 * @return the first free port within the search range
-	 * @throws IOException if no port is available at all
+	 * @throws IOException              if no port is available at all
 	 * @throws IllegalArgumentException if offset is not in range 0..MAX_PORT_OFFSET
 	 */
 	public int getAvailablePort(int offset) throws IOException {
@@ -81,9 +76,8 @@ public class PortUtil {
 		if (offset < 0 || offset > MAX_PORT_OFFSET)
 			throw new IllegalArgumentException("offset too big (must be in range 0.." + MAX_PORT_OFFSET + ")");
 		for (int i = MIN_PORT_NUMBER + offset; i <= MAX_PORT_NUMBER; i++) {
-			if (isAvailable(i)) {
+			if (isAvailable(i))
 				return i;
-			}
 		}
 		throw new IOException("no port available");
 	}
--- src/test/java/de/scrum_master/util/PortUtilTest.groovy
+++ src/test/java/de/scrum_master/util/PortUtilTest.groovy
@@ -6,45 +6,55 @@ import spock.lang.*
  * Unit-test {@link PortUtil} class
  */
 class PortUtilTest extends Specification {
-    static PortUtil portUtil = PortUtil.getInstance()
+	static PortUtil portUtil = PortUtil.getInstance()
 
-    def "get available port"() {
-        when: def port = portUtil.getAvailablePort()
-        then: port >= 1024 && port < 65536
-    }
+	def "get available port"() {
+		when:
+		def port = portUtil.getAvailablePort()
+		then:
+		port >= 1024 && port < 65536
+	}
 
-    def "cannot get unavailable port"() {
-        when:
-            def port = portUtil.getAvailablePort()
-            ServerSocket socket = new ServerSocket(port)
-            socket.setReuseAddress(true)
-        then: !portUtil.isAvailable(port)
-        cleanup: socket?.close()
-    }
+	def "cannot get unavailable port"() {
+		when:
+		def port = portUtil.getAvailablePort()
+		ServerSocket socket = new ServerSocket(port)
+		socket.setReuseAddress(true)
+		then:
+		!portUtil.isAvailable(port)
+		cleanup:
+		socket?.close()
+	}
 
-    def "running out of ports"() {
-        setup:
-            def portUtil = Spy(PortUtil) {
-                isAvailable(!null) >> false
-            }
-        when:
-            portUtil.getAvailablePort()
-        then:
-            def exception = thrown(IOException)
-            exception.message == "no port available"
-    }
+	def "running out of ports"() {
+		setup:
+		def portUtil = Spy(PortUtil) {
+			isAvailable(!null) >> false
+		}
+		when:
+		portUtil.getAvailablePort()
+		then:
+		def exception = thrown(IOException)
+		exception.message == "no port available"
+	}
 
-    @Unroll("illegal port offset #offset")
-    def "illegal port offsets"() {
-        when: portUtil.getAvailablePort(offset)
-        then: thrown(IllegalArgumentException)
-        where: offset << [-1, PortUtil.MAX_PORT_OFFSET + 1]
-    }
+	@Unroll("illegal port offset #offset")
+	def "illegal port offsets"() {
+		when:
+		portUtil.getAvailablePort(offset)
+		then:
+		thrown(IllegalArgumentException)
+		where:
+		offset << [-1, PortUtil.MAX_PORT_OFFSET + 1]
+	}
 
-    @Unroll("illegal port no. #port")
-    def "illegal port numbers"() {
-        when: portUtil.isAvailable(port)
-        then: thrown(IllegalArgumentException)
-        where: port << [-1, PortUtil.MIN_PORT_NUMBER - 1, PortUtil.MAX_PORT_NUMBER + 1]
-    }
+	@Unroll("illegal port no. #port")
+	def "illegal port numbers"() {
+		when:
+		portUtil.isAvailable(port)
+		then:
+		thrown(IllegalArgumentException)
+		where:
+		port << [-1, PortUtil.MIN_PORT_NUMBER - 1, PortUtil.MAX_PORT_NUMBER + 1]
+	}
 }
