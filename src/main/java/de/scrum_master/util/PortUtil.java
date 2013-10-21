2d0dc81303d32aa91669074ff28030e3e775ce26
Alexander Kriegisch <Alexander@Kriegisch.name>
2013-10-21 14:23:31 +0200

    Remove randomness from search offset, make it explicit and documented
    A mandatory randomness is kinda unreproducible. It is better to leave it up to
    the caller if she wants to have a random offset or not. Users who really want to
    check all ports >= 1024 for availability might get the response that none are
    available just because a few thousands of them have been randomly skipped.

--- src/main/java/de/scrum_master/util/PortUtil.java
+++ src/main/java/de/scrum_master/util/PortUtil.java
@@ -9,14 +9,16 @@ import java.net.ServerSocket;
  */
 public final class PortUtil {
 
-	private static int MIN_PORT_NUMBER = 1024;
-	private static int MAX_PORT_NUMBER = 65535;
+	public static final int MIN_PORT_NUMBER = 1024;
+	public static final int MAX_PORT_NUMBER = 65535;
+	public static final int MAX_PORT_OFFSET = MAX_PORT_NUMBER - MIN_PORT_NUMBER - 1;
 
 	/**
 	 * Checks if a specific TCP/UDP port is available
 	 *
 	 * @param port the port to be checked for availability
-	 * @return true if port is available; false if port is unavailable or outside legal range of 1,024..65,535
+	 * @return true if port is available; false if port is unavailable or outside legal range of
+	 * MIN_PORT_NUMBER..MAX_PORT_NUMBER
 	 */
 	public static boolean isAvailable(int port) {
 		if (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER) {
@@ -50,14 +52,26 @@ public final class PortUtil {
 	}
 
 	/**
-	 * Checks all TCP/UDP ports in the range of (1024 + a random offset between 0 and 3,000) and 65,535
-	 * for availability
+	 * Checks all non-privileged TCP/UDP ports in range MIN_PORT_NUMBER..MAX_PORT_NUMBER for availability
 	 * @return the first free port within the search range
 	 * @throws IOException if no port is available at all
 	 */
 	public static int getAvailablePort() throws IOException {
-		int round = (int) Math.round(Math.random() * 3000) % 3000;
-		for (int i = MIN_PORT_NUMBER + round; i <=MAX_PORT_NUMBER; i++) {
+		return getAvailablePort(0);
+	}
+
+	/**
+	 * Checks all non-privileged TCP/UDP ports in range MIN_PORT_NUMBER + offset..MAX_PORT_NUMBER for availability
+	 * @param offset number of ports above MIN_PORT_NUMBER to be skipped during availability check
+	 * @return the first free port within the search range
+	 * @throws IOException if no port is available at all
+	 * @throws IllegalArgumentException if offset is not in range 0..MAX_PORT_OFFSET
+	 */
+	public static int getAvailablePort(int offset) throws IOException {
+		// TODO: Is this method really needed?
+		if (offset < 0 || offset > MAX_PORT_OFFSET)
+			throw new IllegalArgumentException("offset too big (must be in range 0.." + MAX_PORT_OFFSET + ")");
+		for (int i = MIN_PORT_NUMBER; i <=MAX_PORT_NUMBER; i++) {
 			if (isAvailable(i)) {
 				return i;
 			}
